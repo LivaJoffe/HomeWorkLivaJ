8909f2ff28125b7382067498f6bd00fca13cc687
LivaJoffe <liva.joffe@gmail.com>
2021-08-17 20:24:25 +0300

    Homework for FizzBuzz

---
+++ src/main/scala/FizzBuzz.scala
@@ -0,0 +1,19 @@
+object FizzBuzz extends App {
+  //TODO create a FizzBuzz program based on children's game I think from Britain
+  //for numbers 1 to 100 (included!)
+  //print FizzBuzz if number divides evenly by both 5 and 7
+  //print Fizz if number only divides by 5
+  //print Buzz if number divides by 7
+  //print number if number does not divide by 5 or 7
+  //so something like this 1,2,3,4,Fizz,6,Buzz,..... 34,FizzBuzz,36,....
+
+for (i<- 1 to 100) {
+  if (i % 5 == 0 && i % 7 == 0)
+    println(s"$i FizzBuzz")
+  else if (i % 5 == 0 && i % 7 != 0)
+    println(s" $i Fizz")
+  else if (i % 5 != 0 && i % 7 == 0)
+    println(s" $i Buzz")
+  else println(s" $i number")
+}
+}
