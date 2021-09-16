4450c26158aa33fa57eb8803f0213707c396aa7e
LivaJoffe <liva.joffe@gmail.com>
2021-09-16 20:55:05 +0300

    Exercise Clean Will solved

---
+++ src/main/scala/Readingtext/ExerciseSep16CleanWill.scala
@@ -0,0 +1,19 @@
+package Readingtext
+
+
+object ExerciseSep16CleanWill extends App {
+  val src = "src/resources/The Case of the Missing Will.txt"
+  val dst = "src/resources/The Case of the Missing Will_cleaned.txt"
+  val lines = Utilities.getLinesFromFile(src)
+  val matchText = "THE END"
+  val linesNumberWithMatch = lines.zipWithIndex.filter(lineTuple => lineTuple._1.contains(matchText) )
+  val endingIndex = linesNumberWithMatch.head._2+1
+  println(endingIndex)
+  val cleanText =lines.slice(0,endingIndex)
+  Utilities.saveLines(dst,cleanText)
+
+
+  //TODO read src
+  //TODO write to dst the cleaned version without the Project Gutenberg footer
+
+}
